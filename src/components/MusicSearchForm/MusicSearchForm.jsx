e8ec2604007836da55bd1ed6f8a8e1b31225f5a7
Tabor <86973131+TaborMather-dev@users.noreply.github.com>
2021-08-17 16:55:31 -0700

    forgot file from previous commit

--- src/components/MusicSearchForm/MusicSearchForm.jsx
+++ src/components/MusicSearchForm/MusicSearchForm.jsx
@@ -5,16 +5,7 @@ function musicSearchForm(props) {
     render() {
         return(
             <div className="searchformusic-nav">
-            <h3>Search for Music</h3>
-                <form onSubmit = {(event) => this.handleSubmit(event)}>
-                    <label for="song_name">Song Name: </label>
-                    <input type="text" songName="song_name" onChange={this.handleChange} value={this.state.songName}/>
-                    <label for-="artist">Artist: </label>
-                    <input type="text" artistName="artist_name" onChange={this.handleChange} value={this.state.artistName}/>
-                    <label for-="album_name">Album: </label>
-                    <input type="text" albumName="album_name" onChange={this.handleChange} value={this.state.albumName}/>
-                    <button type="submit"> Search </button>
-                </form>
+                <h3>Search for Music</h3>
             </div>
         )
     }
