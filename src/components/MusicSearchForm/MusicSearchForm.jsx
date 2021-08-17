96f40226ffacdd85fb29398d335b66cf50348753
Tabor <86973131+TaborMather-dev@users.noreply.github.com>
2021-08-17 16:53:31 -0700

    made various changes

--- src/components/MusicSearchForm/MusicSearchForm.jsx
+++ src/components/MusicSearchForm/MusicSearchForm.jsx
@@ -1,37 +1,22 @@
-import React, {Component} from 'react';
+import React from 'react';
+import '<./musicSearchForm.css'
     
-class MusicSearchForm extends Component{
-    state = {
-        songName: '',
-        artistName: '',
-        albumName: ''
-    }
-    handleChange = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    };
-    handle
-
-render(){
-    return(
-        <div className="row row-space">
-            <div className="col-md-12" style={{ padding: 0 }}>
-            <h1>Search for Music</h1>
-                <div className="musicsearchform-nav">
-                    <form action="" method="get">
-                        <label for="song_name"> Song Name: </label>
-                        <input type="text" songName="song_name" onChange={this.handleChange} value={this.state.songName}>
-                        <label for-="artist">Artist: </label>
-                        <input type="text" artistName="artist_name"><br>
-                        <label for-="album_name">Album: </label>
-                        <input type="text" albumName="album_name"><br>
-                        <button type="submit"></button>
-                    </form>
-                </div>
+function musicSearchForm(props) {
+    render() {
+        return(
+            <div className="searchformusic-nav">
+            <h3>Search for Music</h3>
+                <form onSubmit = {(event) => this.handleSubmit(event)}>
+                    <label for="song_name">Song Name: </label>
+                    <input type="text" songName="song_name" onChange={this.handleChange} value={this.state.songName}/>
+                    <label for-="artist">Artist: </label>
+                    <input type="text" artistName="artist_name" onChange={this.handleChange} value={this.state.artistName}/>
+                    <label for-="album_name">Album: </label>
+                    <input type="text" albumName="album_name" onChange={this.handleChange} value={this.state.albumName}/>
+                    <button type="submit"> Search </button>
+                </form>
             </div>
-        </div>
         )
-}
+    }
 
 export default musicSearchForm;
