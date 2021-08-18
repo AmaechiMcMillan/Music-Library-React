dc090bf7ec186f72c6c3bd1403fce7614592849e
Tabor <86973131+TaborMather-dev@users.noreply.github.com>
2021-08-18 06:19:11 -0700

    fixed some syntax errors

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
-import './App.css'
-import musicSearchForm from './MusicSearchForm/MusicSearchForm'
+import './App.css';
+import MusicSearchForm from './MusicSearchForm/MusicSearchForm';
 
 class App extends Component {
-    constructor(props){
-        super(props);
-        this.songs = [
-            musicSearchForm({input}
-        ];
-        this.state: {
-
-    }
+    state = {
+        songName: '',
+        artistName: '',
+        albumName: ''
+    };
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    };
+    handleSubmit = (event) => {
+        event.preventDefault();
+        alert(`Song Name: ${this.state.songName} Artist: ${this.state.artistName} Album Name: ${this.state.albumName}`)
+    };
+    componentDidMount(){
+        axios.get('http://'127.0.0.1:8000/music/')
+        .then(response => this.setState({
+            songs: response.data
+        }));
+    };
     render() {
         return (
             <div className="container-fluid"> 
                 <div className="row">
                     <div className="col-md-4">
+                    <MusicSearchForm />
+                        <div className="col-md-4">
+
+                        </div>
                     </div>
                 </div>
             </div>
         )
     }
-}
 export default App;
--- src/components/MusicSearchForm/MusicSearchForm.jsx
+++ src/components/MusicSearchForm/MusicSearchForm.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import '<./musicSearchForm.css'
+
+class MusicSearchForm extends Component {
     
 function musicSearchForm(props) {
     render() {
         return(
             <div className="searchformusic-nav">
                 <h3>Search for Music</h3>
+                <h3>Search for Music</h3>
+                <form onSubmit = {(event) => this.handleSubmit(event)}>
+                    <label for="song_name">Song Name: </label>
+                    <input type="text" songName="song_name" onChange={this.handleChange} value={this.state.songName}/>
+                    <label for-="artist">Artist: </label>
+                    <input type="text" artistName="artist_name" onChange={this.handleChange} value={this.state.artistName}/>
+                    <label for-="album_name">Album: </label>
+                    <input type="text" albumName="album_name" onChange={this.handleChange} value={this.state.albumName}/>
+                    <button type="submit"> Search </button>
+                </form>
             </div>
         )
     }
-
-export default musicSearchForm;
+}
+export default MusicSearchForm;
